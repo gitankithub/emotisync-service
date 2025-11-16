8b562aa7ef320c5aef4364ea0374304f037cab9b
Vaddi <avaddi@teksystems.com>
2025-11-16 22:23:47 +0530

    Added fetch all method to request controller

--- src/main/java/tek/bwi/hackathon/emotisync/restcontroller/RequestController.java
+++ src/main/java/tek/bwi/hackathon/emotisync/restcontroller/RequestController.java
@@ -2,6 +2,7 @@ package tek.bwi.hackathon.emotisync.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tek.bwi.hackathon.emotisync.entities.Reservation;
 import tek.bwi.hackathon.emotisync.entities.ServiceRequest;
 import tek.bwi.hackathon.emotisync.service.ServiceRequestService;
 
@@ -13,6 +14,8 @@ public class RequestController {
     @Autowired private ServiceRequestService serviceRequestService;
     @PostMapping
     public ServiceRequest create(@RequestBody ServiceRequest request) { return serviceRequestService.create(request); }
+    @GetMapping
+    public List<ServiceRequest> getAll() { return serviceRequestService.getAll(); }
     @GetMapping("/user/{guestId}")
     public List<ServiceRequest> getByGuestId(@PathVariable String guestId) { return serviceRequestService.getByGuestId(guestId); }
     @GetMapping("/{id}")
